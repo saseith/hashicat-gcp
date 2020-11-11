396e1bb84e12ab842fc2b5d1b1cded0a44949632
Sebastian Seith <saseith@gmail.com>
2020-11-11 20:48:20 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/SebastianSeith-training/network/google"
+  version = "2.5.0"
+  network_name = "main-vpc"
+  project = var.project
+  
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
